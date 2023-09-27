d69bb3042c2d7631db10b8245f05332818a81e97
facundoVeraa <facu2005vera@gmail.com>
2023-09-27 20:20:03 -0300

    segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,66 +1,44 @@
-/*const puntaje = "1000";
+const NWA = ["Ice Cube", "Dr Dre", "Eazy e", "Mc Ren", "Dj Yella"];
+console.log("1986, se forma el grupo")
+console.log(NWA)
 
-if (puntaje == 1000){
-        console.log ("es igual")
-}*/
+NWA.shift();
+console.log("1989, se divide el grupo")
+console.log(NWA);
 
-/*let NombreUsuario = prompt ("ingresar nombre de usuario")
+NWA.shift();
+console.log("1992, se divide el grupo")
+console.log(NWA);
 
-if (NombreUsuario == ""){
-        alert("no ingresaste nombre de usuario");
+NWA.push("Ice Cube", "Dr Dre");
+        console.log("1995, Se reune el grupo")
+        console.log(NWA)
+
+const rapero1 = {
+        nombre:"Ice Cube",
+        edad:17,
+        nacimiento:1969,
 }
-else{
-        alert("nombre de usuario ingresado" + NombreUsuario);
-}*/
-
-/*const num1 = 10;
-const num2 = 5;
-
-let resultado = num1<num2
-console.log(resultado);
-console.log(typeof resultado);*/
-
-/*for (let i=0; i<=10; i++) {
-        console.log("el valor de x es" + i)
-}*/
-
-
-
-/*let edad = prompt ("Ingresa tu edad")
-
-if (edad <= 18){
-        console.log("Podes ingresar al sitio")
+const rapero2 = {
+        nombre:"Eazy e",
+        edad:22,
+        nacimiento:1964,
 }
-else (edad  >= 18);{
-        console.log("No poder ingresar al sitio")
-}*/
-
-
-
-
-
-
-/*let añoNacimiento = prompt("dame tu año de nacimiento");
-let añoActual = 2023;
-
-let edad = 2023 - añoNacimiento;
-let mayor;
-if (edad >= 18) {
-        mayor = true;
-} else {
-        mayor = false;
+const rapero3 = {
+        nombre:"Dr Dre",
+        edad:21,
+        nacimiento:1965,
 }
-if (mayor == true) {
-        console.log("podes ingresar");
-} else{
-        console.log("no podes ingresar");
-}*/
-
-/*for(let i=0; i<=10; i+=1){
-        console.log("el valor de i es " + i)    
-}*/
-
-//ENTREGA 1
+const rapero4 = {
+        nombre:"Mc Ren",
+        edad:17,
+        nacimiento:1969,
+}
+const rapero5 = {
+        nombre:"Dj Yella",
+        edad:19,
+        nacimiento:1967,
+}
 
 let comando
 while (comando != 'Salir') {
@@ -70,12 +48,9 @@ while (comando != 'Salir') {
         case 'Enviar consulta':
                 let profesor = prompt("Ingrese nombre del profesor");
                 let consulta = prompt("Ingrese su consulta sobre el parcial");
-                 alert("Consulta enviada a,  " + profesor);
+                alert("Consulta enviada a,  " + profesor);
                 break;
         case 'Parciales':
-                for (let i = 1; i <= 3; i++) {
-                alert("Parcial N°" + i);
-                }
                 break;
         case 'Promedio notas':
                 let notaPrimerParcial = parseInt(prompt("Ingrese la nota obtenida en el 1° parcial"));
@@ -86,10 +61,10 @@ while (comando != 'Salir') {
                 alert("Su promedio es " + promedio);
         if (promedio <= 3) {
                 alert("Estas desaprobado!")
-                }
+        }
         else if (promedio >= 4) {
                 alert("Estas aprobado!")
-                }
+        }
         break;
         } 
 }
